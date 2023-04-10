34a23432a071a4878d64fa338efa926859982285
BernardVonBretzel <bernard@vonbretzel.fr>
2023-04-10 22:31:27 +0200

    dico + test onto

--- onto2py.py
+++ onto2py.py
@@ -1,35 +1,50 @@
-import pandas as pd
 from owlready2 import *
 
-# Charger l'ontologie
-onto = get_ontology("test.owl").load()
+# Chargez votre ontologie depuis un fichier local ou une URL
+# Remplacez 'mon_ontologie.owl' par le nom de votre fichier ou l'URL
+onto = get_ontology("Nutriscam.owl").load()
 
-# # Lire le fichier CSV
-# data = pd.read_csv("Tri1.csv")
+import csv
 
-# # Parcourir les lignes du fichier CSV
-# for index, row in data.iterrows():
-#     # Créer une instance pour chaque élément du fichier CSV
-    
-#     # Exemple pour la classe CategoriePNNS
-#     categorie_pnns_instance = onto.CategoriePNNS("CategoriePNNS_" + str(index))
-#     categorie_pnns_instance.pnns_groups_1 = row["pnns_groups_1"]
-#     categorie_pnns_instance.pnns_groups_2 = row["pnns_groups_2"]
+def add_instances_from_csv(file_path):
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # Créez les instances pour chaque classe
+            produit = onto.Produit(namespace=row['code'])
+            valeur_nutritionnelle = onto.ValeurNutritionnelle(namespace=row['code']+"_vn")
+            nutriscore = onto.Nutriscore(namespace=row['code']+"_ns")
+            ecoscore = onto.Ecoscore(namespace=row['code']+"_es")
 
-#     # Ajoutez d'autres instances pour les autres classes ici, en suivant le même modèle que ci-dessus
+            # Remplissez les propriétés de données pour chaque instance
+            produit.code = row['code']
+            produit.url = row['url']
+            produit.product_name = row['product_name']
+            produit.generic_name = row['generic_name']
+            produit.brands = row['brands']
+            produit.categories_en = row['categories_en']
+            produit.ingredients_text = row['ingredients_text']
+            produit.allergens = row['allergens']
+            produit.serving_size = row['serving_size']
 
-#     # Exemple pour la classe Fibres
-#     fibres_instance = onto.Fibres("Fibres_" + str(index))
-#     fibres_instance.fiber = row["fiber_100g"]
-#     fibres_instance.soluble_fiber = row["soluble-fiber_100g"]
-#     fibres_instance.insoluble_fiber = row["insoluble-fiber_100g"]
+            valeur_nutritionnelle.energy_100g = float(row['energy_100g'])
+            valeur_nutritionnelle.fat_100g = float(row['fat_100g'])
+            valeur_nutritionnelle.saturated_fat_100g = float(row['saturated-fat_100g'])
+            valeur_nutritionnelle.carbohydrates_100g = float(row['carbohydrates_100g'])
+            valeur_nutritionnelle.sugars_100g = float(row['sugars_100g'])
+            valeur_nutritionnelle.fiber_100g = float(row['fiber_100g'])
+            valeur_nutritionnelle.proteins_100g = float(row['proteins_100g'])
+            valeur_nutritionnelle.salt_100g = float(row['salt_100g'])
 
-#     # Répétez ce processus pour les autres classes en utilisant les colonnes correspondantes du fichier CSV
+            nutriscore.nutriscore_score = float(row['nutriscore_score'])
+            nutriscore.nutriscore_grade = row['nutriscore_grade']
 
-# # Enregistrez les modifications dans l'ontologie
-# onto.save("test1.owl")
+            ecoscore.ecoscore_score = float(row['ecoscore_score'])
+            ecoscore.ecoscore_grade = row['ecoscore_grade']
 
-# # Afficher les classes de l'ontologie
-print(onto.classes())
+            # Établissez des relations entre les instances
+            produit.aValeurNutritionnelle = [valeur_nutritionnelle]
+            produit.aNutriscore = [nutriscore]
+            produit.aEcoscore = [ecoscore]
 
-
+add_instances_from_csv('Tri1.csv')
